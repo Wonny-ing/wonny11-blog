01c5bc34229edf19aa44b48df2e303f78bd974e3
Wonny-ing <h.wonny11@gmail.com>
2023-04-20 02:54:08 +0900

    Feat: Create Carousel Posts

---
+++ src/components/MultiCarousel.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2,
+  },
+};
+<Carousel responsive={responsive}>
+  <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>
+  <div>Item 4</div>
+</Carousel>;
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function MultiCarousel({ children }: Props) {
+  return (
+    <Carousel infinite autoPlay responsive={responsive} itemClass='m-2'>
+      {children}
+    </Carousel>
+  );
+}
